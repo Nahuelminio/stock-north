ed40a1f062e18cfcef6c970084b48242f40e74ed
nahuelminio <nahuelbenjamin04@gmail.com>
2025-04-29 19:02:45 -0300

    login 4

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const cors = require("cors");
-const testDB = require("./test-db");
 
 app.use(cors());
 app.use(express.json());
